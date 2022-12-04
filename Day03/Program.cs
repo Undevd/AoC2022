1fb3542d5e9b95ed25a6d972fe9d719faf60d46b
Scott Rodgers <scott.r.hulme@live.co.uk>
2022-12-04 21:58:32 +0000

    Adds Day 3 and Day 4

---
+++ Day03/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Day03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = 0;
+            var groups = new List<List<string>>();
+            var group = new List<string>();
+
+            foreach (string backpack in File.ReadLines(@"Backpacks.txt"))
+            {
+                group.Add(backpack);
+
+                if(group.Count == 3)
+                {
+                    groups.Add(group);
+
+                    var match = GroupMatch(group).ToCharArray();
+                    group = new List<string>();
+
+                    total += ToPriority(match.FirstOrDefault());
+                }
+            }
+            Console.WriteLine(total);
+        }
+
+        public static List<string> ToCompartments(string backpack)
+        {
+            return new List<string>
+            {
+                backpack.Substring(0, backpack.Length / 2),
+                backpack.Substring(backpack.Length /2)
+            };
+        }
+
+        public static string FindMatch(string compartmentA, string compartmentB)
+        {
+            return compartmentA.AsEnumerable().Where(x => compartmentB.Contains(x)).FirstOrDefault().ToString();
+        }
+
+        public static string GroupMatch(List<string> backpacks)
+        {
+
+            var matches = backpacks[0].AsEnumerable().Where(x => backpacks[1].Contains(x)).ToList();
+            return matches.AsEnumerable().Where(x => backpacks[2].Contains(x)).FirstOrDefault().ToString();
+        }
+
+        public static int ToPriority(char item)
+        {
+            var code = (int)item;
+
+            if(code >= (int)'a' && code <= (int)'z')
+            {
+                return (int)item - ((int)'a' - 1);
+            }
+            else
+            {
+                return (int)item - (int)'A' + 27;
+            }
+            
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Day04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var countA = 0;
+            var countB = 0;
+            foreach (string assignmentStr in File.ReadLines(@"Assignments.txt"))
+            {
+                var elves = new List<Assignment>();
+                var assignments = assignmentStr.Split(",");
+                foreach (var a in assignments)
+                {
+                    elves.Add(new Assignment(a));
+                }
+
+                if (DoesOverlapComplete(elves[0], elves[1]))
+                {
+                    countA += 1;
+                    countB += 1;
+                }
+                else if(DoesOverlapPartial(elves[0], elves[1]))
+                {
+                    countB += 1;
+                }
+            }
+            Console.WriteLine($"Complete Overlap: {countA}");
+            Console.WriteLine($"Partial Overlap: {countB}");
+        }
+
+        private static bool DoesOverlapComplete(Assignment a, Assignment b)
+        {
+            var largest = a.Range >= b.Range ? a : b;
+            var smallest = largest.Equals(a) ? b : a;
+
+            return smallest.Lower >= largest.Lower && smallest.Upper <= largest.Upper ? true : false;
+        }
+
+        private static bool DoesOverlapPartial(Assignment a, Assignment b)
+        {
+            var lowest = a.Lower <= b.Lower ? b : a;
+            var highest = lowest.Equals(a) ? b : a;
+
+            return (highest.Lower >= lowest.Lower && highest.Lower <= lowest.Upper) ||
+                    (highest.Upper >= lowest.Lower && highest.Upper <= lowest.Upper) ? true : false;
+        }
+    }
+
+    public class Assignment
+    {
+        public Assignment(string rangeStr)
+        {
+            var range = rangeStr.Split('-');
+
+            this.Lower = int.Parse(range[0]);
+            this.Upper = int.Parse(range[1]);
+            Range = Upper - Lower;
+
+        }
+
+        public int Lower;
+        public int Upper;
+        public int Range;
+    }
+}
